1943402a2812817832f5d83375475ee123659138
morrisjdev <morrisj@live.de>
2020-02-02 20:23:39 +0100

    updated example to work with example server

--- src/index.js
+++ src/index.js
@@ -1,34 +1,163 @@
-import React, { useState, useEffect } from 'react';
+import React, {useEffect, useState} from 'react';
 import ReactDOM from 'react-dom';
-import { SapphireDb } from 'sapphiredb';
+import {SapphireDb} from 'sapphiredb';
+import {filter, map, switchMap, take} from "rxjs/operators";
+import {combineLatest, ReplaySubject} from "rxjs";
+
+const useObservable = observable => {
+    const [state, setState] = useState();
+
+    useEffect(() => {
+        const sub = observable.subscribe((v) => setState({ value: v }));
+        return () => sub.unsubscribe();
+    }, [observable]);
+
+    return state;
+};
+
+const db = new SapphireDb({
+    useSsl: true,
+    serverBaseUrl: 'sapphiredb-demo.azurewebsites.net',
+    connectionType: 'websocket'
+});
+
+const exampleCollection = db.collection('basic.examples');
+const examples$ = exampleCollection.values();
+
+const userCollection = db.collection('chat.users');
+const users$ = userCollection.values();
+const currentUser$ = new ReplaySubject();
+const chatPartners$ = combineLatest([users$, currentUser$]).pipe(
+    map(([users, currentUser]) => users.filter(u => u.id !== currentUser.id))
+);
+
+const chatPartner$ = new ReplaySubject();
+
+const messageCollection$ = combineLatest([currentUser$, chatPartner$]).pipe(
+    filter(([currentUser, chatPartner]) => !!currentUser && !!chatPartner),
+    map(([currentUser, chatPartner]) => {
+        return db.collection('chat.messages').where([
+            [['ownerId', '==', currentUser.id], 'and', ['receiverId', '==', chatPartner.id]],
+            'or',
+            [['ownerId', '==', chatPartner.id], 'and', ['receiverId', '==', currentUser.id]]
+        ]);
+    })
+);
+const messages$ = messageCollection$.pipe(switchMap(c => c.values()));
 
 function App() {
-  const [values, setValues] = useState();
+    /* Basic example */
+    const examples = useObservable(examples$);
 
-  const db = new SapphireDb({
-    apiSecret: 'pw1234',
-    apiKey: 'webapp',
-    useSsl: false,
-    serverBaseUrl: 'localhost:5000'
-  });
+    function addExample() {
+        exampleCollection.add({
+            content: prompt('Content of new example:')
+        });
+    }
 
-  useEffect(() => {
-    const subscription = db.collection('demo.entries').values().subscribe(setValues);
-    return () => subscription.unsubscribe();
-  }, []);
+    function updateExample(example) {
+        exampleCollection.update({
+            ...example,
+            content: prompt('New content of example:')
+        });
+    }
 
-  return (
-      <div className="App">
-        <h1>SapphireDb with React</h1>
-        <ul>
-          {values?.map(item => (
-              <li>{item.content}</li>
-          ))}
-        </ul>
-      </div>
-  );
+    function removeExample(example) {
+        exampleCollection.remove(example);
+    }
+
+    /* Chat */
+    const users = useObservable(users$);
+
+    function createUser() {
+        userCollection.add({
+            username: prompt('New username: ') || 'default'
+        });
+    }
+
+    const [userState, setUserState] = useState();
+    let chatPartners = useObservable(chatPartners$);
+
+    function setUser(user) {
+        setUserState({ currentUser: user, chatPartner: null });
+        chatPartner$.next(null);
+        currentUser$.next(user);
+    }
+
+    function setChatPartner(user) {
+        setUserState({ ...userState, chatPartner: user });
+        chatPartner$.next(user);
+    }
+
+    const messages = useObservable(messages$);
+
+    const [messageState, setMessageState] = useState();
+
+    function sendMessage() {
+        messageCollection$.pipe(
+            take(1)
+        ).subscribe(collection => {
+           collection.add({
+               ownerId: userState.currentUser.id,
+               receiverId: userState.chatPartner.id,
+               content: messageState
+           });
+           setMessageState('');
+        });
+    }
+
+    return (
+        <div className="App">
+            <h1>Example React application using SapphireDb</h1>
+            <h2>Basic example</h2>
+
+            {examples?.value.map(example => (
+                <div key={example.id}>
+                    {JSON.stringify(example)}
+                    <button onClick={() => updateExample(example)}>Update</button>
+                    <button onClick={() => removeExample(example)}>Remove</button>
+                </div>
+            ))}
+
+            <button onClick={addExample}>Add</button>
+
+            <h2>Chat</h2>
+            <h3>Users</h3>
+
+            {users?.value.map(user => (
+                <button key={user.id} onClick={() => setUser(user)}>{user.username}</button>
+            ))}
+
+            <button onClick={createUser}>Create user</button>
+
+            {userState && userState.currentUser ? (
+                <div>
+                    <h3>You are {userState.currentUser.username}</h3>
+                    <h4>Select chat partner</h4>
+                    {chatPartners?.value.map(partner => (
+                        <button key={partner.id} onClick={() => setChatPartner(partner)}>{partner.username}</button>
+                    ))}
+
+                    { userState.chatPartner ? (
+                        <div>
+                            <h3>Chat between '{userState.currentUser.username}' and '{userState.chatPartner.username}'</h3>
+                            <h4>Message</h4>
+                            {messages?.value.map(message => (
+                                <div style={{textAlign: message.ownerId === userState.currentUser.id ? 'right' : 'left'}}>{message.content} ({message.createdOn})</div>
+                            ))}
+                            <label>
+                                New Message:
+                                <input value={messageState} onChange={(event) => setMessageState(event.target.value)} />
+                            </label>
+                            <button onClick={sendMessage}>Send</button>
+                        </div>
+                    ) : ('')}
+                </div>
+            ) : ('')}
+        </div>
+    );
 }
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<App/>, rootElement);
 
